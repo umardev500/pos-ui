5f2a0fb4cedf5d9e62b559a6e627f717e9bd414e
Umar <umar.dev500@gmail.com>
2025-04-26 21:41:05 +0700

    use formik

--- src/screens/auth/login-screen.tsx
+++ src/screens/auth/login-screen.tsx
@@ -1,22 +1,42 @@
 import {authService} from '@app/services/auth';
 import {useMutation} from '@tanstack/react-query';
-import {Button} from 'react-native';
+import {Formik} from 'formik';
+import {Button, TextInput} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 export const LoginScreen = () => {
   const {mutate} = useMutation({
-    mutationFn: () => authService.login('admin@gmail.com', 'admin'),
+    mutationFn: (values: {email: string; password: string}) =>
+      authService.login(values.email, values.password),
     onSuccess: data => console.log(data),
     onError: error => console.log(error),
   });
 
-  const handleLogin = () => {
-    mutate();
-  };
-
   return (
     <SafeAreaView>
-      <Button title="Login" onPress={handleLogin} />
+      <Formik
+        initialValues={{email: '', password: ''}}
+        onSubmit={values => {
+          mutate(values);
+        }}>
+        {({handleChange, handleBlur, handleSubmit, values}) => (
+          <>
+            <TextInput
+              placeholder="Email"
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+              value={values.email}
+            />
+            <TextInput
+              placeholder="Password"
+              onChangeText={handleChange('password')}
+              onBlur={handleBlur('password')}
+              value={values.password}
+            />
+            <Button title="Login" onPress={handleSubmit as any} />
+          </>
+        )}
+      </Formik>
     </SafeAreaView>
   );
 };
