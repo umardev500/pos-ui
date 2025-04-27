50d2d7a6e41d818e969ff1b6da29f373dd4440ac
Umar <umar.dev500@gmail.com>
2025-04-27 21:46:28 +0700

    mod: add handler into header

--- src/components/organisms/header/main-header.organism.tsx
+++ src/components/organisms/header/main-header.organism.tsx
@@ -1,6 +1,6 @@
 import {Icon, Input} from '@app/components/atoms';
 import React from 'react';
-import {View} from 'react-native';
+import {Pressable, View} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 type Props = {};
@@ -8,17 +8,25 @@ type Props = {};
 export const MainHeader: React.FC<Props> = ({}) => {
   const {top} = useSafeAreaInsets();
 
+  const handleChange = (text: string) => {
+    // TODO
+  };
+
   return (
     <View style={{paddingTop: top}}>
       <View className="flex-row items-center justify-between px-4 pt-4 pb-6 gap-6">
         <View className="flex-1 flex-row items-center gap-4">
           <Icon name="menu" />
-          <Input trailingIcon="search" placeholder="Search" size="md" />
+          <Input onChangeText={handleChange} trailingIcon="search" placeholder="Search" size="md" />
         </View>
 
         <View className="flex-row items-center gap-4">
-          <Icon name="assigment" />
-          <Icon name="notification" />
+          <Pressable onPress={() => {}}>
+            <Icon name="assigment" />
+          </Pressable>
+          <Pressable onPress={() => {}}>
+            <Icon name="notification" />
+          </Pressable>
         </View>
       </View>
     </View>
