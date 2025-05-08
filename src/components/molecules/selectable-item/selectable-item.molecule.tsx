da7ceb738e17e0e5c6ad5668e7a3e87a9bf7d13a
Umar <umar.dev500@gmail.com>
2025-05-08 10:12:39 +0700

    support name or label for display text selectable item

--- src/components/molecules/selectable-item/selectable-item.molecule.tsx
+++ src/components/molecules/selectable-item/selectable-item.molecule.tsx
@@ -4,7 +4,8 @@ import {Text, TouchableOpacity} from 'react-native';
 
 export type SelectableItemType = {
   id: number | string;
-  label: string;
+  label?: string;
+  name?: string;
 };
 
 type SelectableItemProps<T extends SelectableItemType> = {
@@ -14,13 +15,15 @@ type SelectableItemProps<T extends SelectableItemType> = {
 };
 
 export const SelectableItem = <T extends SelectableItemType>({item, selected, onSelect}: SelectableItemProps<T>) => {
+  const displayText = item.label ?? item.name ?? 'Unnamed';
+
   return (
     <TouchableOpacity
       onPress={() => onSelect(item)}
       className={clsx('border border-dashed border-gray-300 rounded-xl px-4 py-3', {
         'bg-gray-100': selected,
       })}>
-      <Text className="text-sm text-gray-800 font-medium">{item.label}</Text>
+      <Text className="text-sm text-gray-800 font-medium">{displayText}</Text>
     </TouchableOpacity>
   );
 };
