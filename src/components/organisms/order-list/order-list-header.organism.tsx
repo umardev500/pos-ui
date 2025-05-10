0cd3e6bdb7aff0628ea5df245f9e45ff98602be9
Umar <umar.dev500@gmail.com>
2025-05-10 11:11:58 +0700

    add handler to navigate to customer list

--- src/components/organisms/order-list/order-list-header.organism.tsx
+++ src/components/organisms/order-list/order-list-header.organism.tsx
@@ -2,17 +2,27 @@ import {Icon, Input} from '@app/components/atoms';
 import {OrderTypeList} from '@app/components/organisms/order-type-list';
 import {useCartStore} from '@app/stores';
 import {colors} from '@app/styles';
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
 
 type Props = {};
 
 export const OrderListHeader: React.FC<Props> = ({}) => {
+  const navigation = useNavigation();
+
   const handleChangeText = (text: string) => {
     useCartStore.getState().setCustomerName(text);
   };
 
   const customerName = useCartStore(state => state.customerName);
+
+  // ————————————————————————————————————————————————
+  // 🛠 Handlers
+  // ————————————————————————————————————————————————
+  const handleNavigateToCustomerList = () => {
+    navigation.navigate('CustomerList');
+  };
 
   return (
     <>
@@ -29,7 +39,9 @@ export const OrderListHeader: React.FC<Props> = ({}) => {
           />
         </View>
 
-        <TouchableOpacity className="bg-gray-100 rounded-xl w-10 h-10 items-center justify-center">
+        <TouchableOpacity
+          onPress={handleNavigateToCustomerList}
+          className="bg-gray-100 rounded-xl w-10 h-10 items-center justify-center">
           <Icon name="add" size={20} color={colors.gray[600]} />
         </TouchableOpacity>
       </View>
