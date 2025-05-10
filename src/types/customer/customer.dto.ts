97853eefcb096b1335f5332fccca9a9af1906051
Umar <umar.dev500@gmail.com>
2025-05-10 10:28:07 +0700

    refactor customer dto type

--- src/types/customer/customer.dto.ts
+++ src/types/customer/customer.dto.ts
@@ -1,6 +1,11 @@
-export interface CustomerDTO {
-  id?: number;
-  name?: string;
-  email?: string;
-  phone?: string;
-}
+export type CustomerDTO = {
+  id: number;
+  merchant_id: number;
+  name: string;
+  email: string;
+  phone: string | null;
+  address: string | null;
+  points: number;
+  created_at: string; // or Date if you'd prefer to work with Date objects
+  updated_at: string; // or Date if you'd prefer to work with Date objects
+};
